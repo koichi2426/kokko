857bee1a40fd1007c34dac22dbca493a8dc8b03e
koichi <111332153+koichi2426@users.noreply.github.com>
2025-05-13 21:16:48 +0900

    config設定
    http://yourdomain.com/ にアクセスすると
    
    内部的に http://yourdomain.com/front が表示されます（URLは変わらず / のまま）

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/",         // アクセスされた / を
+        destination: "/front", // /front にリライト
+      },
+    ];
+  },
 };
 
 export default nextConfig;
