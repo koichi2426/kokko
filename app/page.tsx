7404943aee7f3052d52fbe2c4def01fc0b39ae96
koichi <111332153+koichi2426@users.noreply.github.com>
2025-05-14 01:20:04 +0900

    写真の追加と調整

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,16 @@ import Footer from "./components/layouts/Footer/Footer";
 import CoccoCharacter from "./features/home/components/CoccoCharacter/CoccoCharacter";
 import PoemBubble from "./features/home/components/PoemBubble/PoemBubble";
 
-export default function home() {
+export default function Home() {
   return (
     <>
       <Header />
-      <main className="min-h-screen bg-[#40494F] text-white flex flex-col items-center gap-6 p-4">
-        <div className="flex flex-row items-center gap-6">
-          <CoccoCharacter />
-          <PoemBubble poem="晴れ空に　汗ばむ午後の　東京風" />
+      <main className="min-h-screen bg-[#40494F] text-white flex flex-col">
+        <div className="flex-grow flex items-start justify-center pt-32">
+          <div className="flex flex-row items-center gap-6">
+            <CoccoCharacter />
+            <PoemBubble poem="晴れ空に　汗ばむ午後の　東京風" />
+          </div>
         </div>
       </main>
       <Footer />
